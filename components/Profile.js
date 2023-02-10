329c9ac40cf6e294350db084cd190ad808886731
Dolorious <doroteyastoyanova555@gmail.com>
2023-02-10 14:22:46 +0000

    added updated todoapp

--- components/Profile.js
+++ components/Profile.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, TextInput } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Animated } from 'react-native';
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import ProgressBar from './ProgressBar';
+
 
 
 const Profile = () => {
   const [level, setLevel] = useState(1);
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const styles = StyleSheet.create  = {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white'
+    },
+    text: {
+      fontSize:20,
+      margin: 10
+    },
+    image: {
+      width: 500,
+      height: 300,
+      alignSelf: 'center',
+      margin: 20
+    }
+  };
 
 
   return (
-    <View style={{ flex: 1, absolute:0 ,alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      alignSelf:'auto',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#CEB0FF'
+    }}>
+
        <Text style={{fontSize:40,fontFamily: 'Gideon Roman'}}> Level:</Text>
   <Text style={{fontSize:100,fontFamily: 'Gideon Roman'}}>{level}</Text>
+  
+  <ProgressBar style={{ width: 50, height: 50, resizeMode:'contain '}} />
+  <Text style={styles.text}>{`Welcome, ${name}!`}</Text>
+
 </View>
+
+
+
+
 
       
   );
--- components/Todo.js
+++ components/Todo.js
@@ -1,107 +1,189 @@
+import {Button, Platform, ScrollView, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View} 
+from 'react-native'
 
-import { View, Text, TextInput, Button, Picker } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react'
+
+
+let id = 0;
+
+const Todo = props => (
+    <View style = {styles.item}>
+        <View style = {styles.left}>
+          <Switch value = {props.todo.checked} onValueChange = {props.onToggle}/>
+          <Text style={styles.text}>{props.todo.text}</Text>
+        </View>
+        <View>
+            <Button  
+              title = "Delete" 
+              onPress = { props.onDelete} 
+              style = {styles.circular}
+
+            />
+        </View>
+    </View>
+)
+
+export default class App extends React.Component{
+    constructor()
+    {
+      super();
+      this.state = {
+        text: "",
+        todos: [],
+      };
+    }
+
+    // add TODO
+    addTodo()
+    {
+      id++;
+      if(this.state.text !== "")
+      {
+        this.setState({
+          todos: [
+            ...this.state.todos, { id: id++, text: this.state.text, checked: false }
+          ]
+        });
+
+        // Empty the input
+        this.setState({
+          text: ""
+        })
+      }
+    }
+
+    // remove TODO
+    removeTodo(id){
+      this.setState({
+        todos: this.state.todos.filter(todo => todo.id !== id)
+      });
+    }
+    // Handle checked todo
+    toggleTodoState(id)
+    {
+      this.setState({
+        todos: this.state.todos.map(todo => {
+          if (todo.id !== id) return todo;
+          return {
+            id: todo.id,
+            text: todo.text,
+            checked: !todo.checked,
+          };
+        })
+      })
+    }
+    render()
+    {
+      return (
+        <View style = {[styles.fill, styles.AppContainer]}>
+          <View style = {styles.HeaderContainer}>
+            <View style={styles.counters}>
+               <Text>Tasks Count:</Text>
+              <Text style = {styles.count}>{this.state.todos.length}</Text>
+            </View>
+            <View style={styles.counters}>
+               <Text>Uncompleted Tasks Count:</Text>
+              <Text style = {styles.count}>{this.state.todos.filter(todo => !todo.checked).length}</Text>
+            </View>
+          </View>
+          <View style = {[styles.inputContainer]}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter TODO Please"
+              keyboardType = "alphanumeric"
+              value = {this.state.text}
+              onChangeText={(text) => this.setState({text})}
+            />
+            <Button 
+              style = {styles.button}
+              title = "Add"
+              onPress={() => this.addTodo()} 
+            />
+          </View>
+          <ScrollView style = {styles.fill}>
+            {this.state.todos.map( todo => (
+               <Todo
+                todo = {todo}
+                onDelete = {() => this.removeTodo(todo.id)} 
+                onToggle = {() => this.toggleTodoState(todo.id)}
+                key = {todo.id} 
+               />
+            ))}
+          </ScrollView>
+        </View>
+      );
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#b4848a',
+  AppContainer: {
+    paddingTop: 45,
+    backgroundColor: "#pink",
   },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
+  HeaderContainer: {
+    flexDirection: 'row', 
+    justifyContent: "space-around",
+    paddingVertical: 15,
+    paddingHorizontal:10,
+    margin: 20,
+    borderRadius: 20,
+    backgroundColor: "#BFB9FA",
   },
-  todoInput: {
-    height: 40,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
+  fill: {
+    flex: 1
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingVertical: 10
+  },
+  counters: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  count: {
+    fontWeight: "bold",
+    marginHorizontal: 5,
+    color: "#55bcf6",
+
+  },
+  input: {
+    height: 45,
+    width: 300,
+    borderWidth: 0.5,
     padding: 10,
-    marginBottom: 10,
+    borderRadius: 50,
   },
-  todoItem: {
+  button: {
+    height: 40,
+   padding: 20,
+    borderRadius: 10
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 20,
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 5,
     marginBottom: 10,
   },
-  todoText: {
-    fontSize: 16,
-    marginRight: 10,
+  left: {
+    flexDirection: 'row',
+    alignItems: "center",
+    flexWrap: "wrap"
   },
-  circleButton: {
-    position: 'absolute',
-    bottom: 50,
-    right : 50,
-    alignSelf: 'flex-end',
-
+  circular:{
+    height: 40,
+    padding: 10,
+    borderColor: '#55bcf6',
+    borderWidth: 2,
+    borderRadius: 5 
   },
-
-
-});
-
-
-const Todo = () => {
-  const [todoItems, setTodoItems] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-  
-  const addTodo = () => {
-    const newTodoItem = {
-      text: newTodo,
-      completed: false,
-    };
-    setTodoItems([...todoItems, newTodoItem]);
-    setNewTodo('');
-
-
-  };
-
-  const deleteTodo = (index) => {
-    // Create a new array with the item at the given index removed
-    const newTodoItems = [...todoItems.slice(0, index), ...todoItems.slice(index + 1)];
-    // Update the todoItems state with the new array
-    setTodoItems(newTodoItems);
-  };
-  
-
-  
-  
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tasks</Text>
-      <TextInput
-        style={styles.todoInput}
-        placeholder="Feed Tamashigo"
-        value={newTodo}
-        color="#000"
-        onChangeText={text => setNewTodo(text)}
-
-      />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}/>
-      <Button onPress={addTodo} color = "black" icon="add" title="Add" />
-      <Button onPress={() => deleteTodo(index)} color = "red" icon="delete" title="Delete" />
-   
-
-{todoItems.map((item, index) => (
-    <View key={index} style={styles.todoItem}>
-      <Text style={styles.todoText}>{item.text}</Text>
-      
-      
-      <Button onPress={() => deleteTodo(index)} color = "red" icon="delete" title="Delete" />
-    </View>
-   
-  
-  ))}
-    
-
-
-</View>
-
-  );
-};
-export default Todo ;
-
+  text: {
+    maxWidth: "80%",
+    marginLeft: 10
+  },
+})
