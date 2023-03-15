1f91392e2a53aee27a2a969791fdfc3cc687a4e2
Dolorious <doroteyastoyanova555@gmail.com>
2023-03-15 12:27:29 +0000

    separated into three stacks Login, Main Page, Setttings

--- StackNavigator.js
+++ StackNavigator.js
@@ -6,7 +6,8 @@ import SignUp from "./pages/SignUp";
 import ForgotPass from "./pages/ForgotPass";
 import BeginningPage from './pages/BeginningPage';
 import Login from './pages/Login';
-
+import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 const Stack = createStackNavigator(); // Create a StackNavigator
 
@@ -14,18 +15,21 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Group>
-        <Stack.Screen name = "Beginning" component={BeginningPage} />
-        <Stack.Screen name = "Login" component={Login} />
-        <Stack.Screen name = "SignUp" component={SignUp} />
-        <Stack.Screen name = "ForgotPass" component={ForgotPass} />
-        <Stack.Screen name="Character Name" component={Namechar} />
-        
-       
+        <Stack.Screen name="Beginning" component={BeginningPage} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="ForgotPass" component={ForgotPass} />
+      </Stack.Group>
 
-      </Stack.Group> 
+      <Stack.Group>
+        <Stack.Screen name="Character Name" component={Namechar} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Group>
+      <Stack.Group>
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Group>
     </Stack.Navigator>
   );
 };
-
 
 export default StackNavigator;
