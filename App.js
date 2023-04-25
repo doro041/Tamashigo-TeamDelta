1c146f57ce4953279e581a6d7cd3a9dacbf47725
Derpimon <u07zk20@abdn.ac.uk>
2023-04-25 19:05:17 +0100

    Login changed

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import StackNavigator from "./StackNavigator";
-import { StatusBar } from 'react-native'; // Import the StatusBar component from the react-native package
-import {SafeAreaView, StyleSheet, Text,View } from 'react-native';
-import { getAuth, signInWithCustomToken, onAuthStateChanged } from 'firebase/auth';
+import { StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { firebaseConfig } from './firebase.js';
-
-
-
 
 const App = () => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Check for saved authentication token
+    // Check for saved user
     const checkAuth = async () => {
-      const authToken = await AsyncStorage.getItem('authToken');
-      if (authToken) {
-        const auth = getAuth();
-        try {
-          const userCredential = await signInWithCustomToken(auth, authToken);
-          setUser(userCredential.user);
-        } catch (error) {
-          console.log(error);
-        }
+      const storedUser = await AsyncStorage.getItem('user');
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
       }
       setIsLoading(false);
     };
@@ -40,10 +31,10 @@ const App = () => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        AsyncStorage.setItem('authToken', user.refreshToken);
+        AsyncStorage.setItem('user', JSON.stringify(user));
       } else {
         setUser(null);
-        AsyncStorage.removeItem('authToken');
+        AsyncStorage.removeItem('user');
       }
     });
     return unsubscribe;
@@ -56,16 +47,14 @@ const App = () => {
       </View>
     );
   }
-return (
-    <SafeAreaView style={{flex:1}}>
-<NavigationContainer>
-<StackNavigator />
-</NavigationContainer>
-</SafeAreaView>
 
-);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
 };
 
-
 export default App;
-
